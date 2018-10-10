97ab459b2d453bb7d9c1e317a6ea60e222adf0fd
cifulton <cfulton@dunwoody.edu>
2018-10-10 10:05:46 -0500

    pushing up price_report

--- price_report.java
+++ price_report.java
@@ -10,7 +10,7 @@ public class  reviewchapt5 {
        int accumPrice = 0;
        String HEADING1 = "MONTH-END SALES REPORT";
        String HEADING2 = "Address             Price";
-       String QUIT = "ZZZ"; //Sentinel value
+       String QUIT = "QUIT"; //Sentinel value
 
        System.out.println(HEADING1);
        System.out.println(HEADING2);
@@ -18,10 +18,21 @@ public class  reviewchapt5 {
        System.out.println("Enter address of property");
        address = inputValue.nextLine();
 
+       //Looping structure
        while(address.equalsIgnoreCase(QUIT)){
            System.out.println("Enter price of property");
            price = inputValue.nextLine();
+
+           System.out.println(address + " "+ price);
+           accumPrice = accumPrice + price;
+
+           System.out.println("Please enter another address or enter 'QUIT' to exit program");
+           address = inputValue.next();
+
        }
+
+       //Sequence
+       System.out.println("Total accumulative of all entries is " + accumPrice);
 
     }
     
