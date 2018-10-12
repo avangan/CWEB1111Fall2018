2875912e845b6efe12d54b5a7f84a4923033fba1
cifulton <cfulton@dunwoody.edu>
2018-10-12 10:03:03 -0500

    adding adjustments to price_report.java file

--- price_report.java
+++ price_report.java
@@ -1,13 +1,14 @@
 import java.util.*;
 import java.lang.Math;
 
-public class  reviewchapt5 {
+public class  price_report {
     private static Scanner inputValue = new Scanner(System.in);
 
     public static void main(String[] args) {
        String address;
        double price;
-       int accumPrice = 0;
+       int totalIterations = 0;
+       double accumPrice = 0;
        String HEADING1 = "MONTH-END SALES REPORT";
        String HEADING2 = "Address             Price";
        String QUIT = "QUIT"; //Sentinel value
@@ -19,20 +20,22 @@ public class  reviewchapt5 {
        address = inputValue.nextLine();
 
        //Looping structure
-       while(address.equalsIgnoreCase(QUIT)){
+       while(!(address.equalsIgnoreCase(QUIT))){
+            totalIterations++;
            System.out.println("Enter price of property");
-           price = inputValue.nextLine();
+           price = inputValue.nextDouble();
 
            System.out.println(address + " "+ price);
            accumPrice = accumPrice + price;
 
            System.out.println("Please enter another address or enter 'QUIT' to exit program");
            address = inputValue.next();
-
+           inputValue.nextLine();
+          
        }
 
        //Sequence
-       System.out.println("Total accumulative of all entries is " + accumPrice);
+       System.out.println("Total accumulative of all entries is " + accumPrice + " based on " + totalIterations + " entries");
 
     }
     
